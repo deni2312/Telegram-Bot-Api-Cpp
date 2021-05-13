a326b075eebca660acbbf8f88c6912d43d6f0a72
Deni2312 <denisbeqi2312@gmail.com>
2021-05-13 17:34:33 +0200

    Version 1.1 update

---
+++ telegram/main.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+#include "include/TelegramAPI.h"
+
+void sendSomething(const Telegram::Bot::Types::API& api, const Telegram::Bot::Types::MessageReceive& message) {
+}
+
+int main()
+{
+	Telegram::Bot::Connector handler("your-token");
+	handler.callback(sendSomething);
+}
